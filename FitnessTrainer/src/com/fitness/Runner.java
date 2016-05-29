7c4c9373e6f42da80a9921957abaecc0e106948d
sashok58 <sashok58@gmail.com>
2016-05-30 00:25:42 +0300

    A.MITSIH COMMIT

--- FitnessTrainer/src/com/fitness/Runner.java
+++ FitnessTrainer/src/com/fitness/Runner.java
@@ -12,28 +12,7 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        /*Person alex = new Person();
-        alex.setName("Alex");
-        alex.setWeight(90);
-        alex.setPersentFat(10);
-        alex.setFocus(Focus.MUSCLE_GROWHT);
-        alex.setExpiriense(Expiriense.PROFESSIONAL);
-        alex.weightExeption();
-        System.out.println(alex);
-        List<Exercise> exercises = new ArrayList<>();
-        exercises.add(Exercise.ARM);
-        exercises.add(Exercise.BODY);
-        exercises.add(Exercise.LEGS);
 
-
-        for (int i = 0; i < exercises.size(); i++) {
-            Utils utils = new Utils(alex, exercises.get(i));
-            System.out.println(utils);
-
-        }*/
-/*double test = 1.0123456789;
-        BigDecimal bd = new BigDecimal(test);
-        System.out.println(bd.setScale(2).doubleValue());*/
         List<Person> list = generatePersonList();
         list = generateExercise(list);
         list = generateStatistic(list);
